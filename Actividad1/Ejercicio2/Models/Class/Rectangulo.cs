ee6faa40db3585008d55ce5f1198f91b823ffc6d
Sedangu <sedangu@gmail.com>
2025-08-10 14:12:54 -0300

    Subiendo Ejercici2

---
+++ Actividad1/Ejercicio2/Models/Class/Rectangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_solo.Models.Class
+{
+    internal class Rectangulo
+    {
+        public double Largo { get; set; }
+        public double Ancho { get; set; }
+
+        public Rectangulo(double ancho, double largo)
+        {
+            Largo = largo;
+            Ancho = ancho;
+        }
+        public double CalcularArea()
+        {
+            return Largo * Ancho;
+        }
+
+        public string Describir()
+        {
+            var culture = CultureInfo.InvariantCulture;
+
+            return $@"
+{{ 
+  ""Tipo"": ""Rectangulo"", 
+  ""Largo"": {Largo.ToString("f2", culture)}, 
+  ""Ancho"": {Ancho.ToString("f2", culture)}, 
+  ""Area"": {CalcularArea().ToString("f2", culture)}
+}}";
+        }
+
+    }
+}
